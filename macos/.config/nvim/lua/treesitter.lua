d0be587a9ddda8258bc1aca6ded1663263cc0205
Patricio Barletta <patricio.barletta@mulesoft.com>
2025-03-22 11:37:43 -0700

    configs

--- macos/.config/nvim/lua/treesitter.lua
+++ macos/.config/nvim/lua/treesitter.lua
@@ -1,6 +1,6 @@
 -- Treesitter Plugin Setup 
 require('nvim-treesitter.configs').setup {
-  ensure_installed = { "lua", "rust", "toml" },
+  ensure_installed = { "lua", "rust", "toml", "go" },
   auto_install = true,
   highlight = {
     enable = true,
